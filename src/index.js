29a64fc4551996c76b3cf617aa962974d9b0688a
magna <julijagarkusha@gmail.com>
2023-04-19 12:36:39 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// External deps
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//Internal deps
+import App from "./components/App";
+
+// Local deps
+import './index.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
